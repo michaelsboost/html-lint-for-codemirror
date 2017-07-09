d932d562b2459bf736bbb7c4b2ac6a0d9b87c6a0
Michael <MIchael@MichaelsMacBook.attlocal.net>
2017-07-09 14:07:09 -0500

    republished for Github Pages

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Want to use [HTMLHint](http://htmlhint.com/js/htmlhint.js) for syntax validation
 
 **NOTE:** If you're using [Ace Editor](http://ace.c9.io/) please refer to [htmlhint.com](http://htmlhint.com/). This release is strictly a [Codemirror](http://codemirror.net/) Integration.
 
+[Try it on kodeWeave!](https://mikethedj4.github.io/kodeWeave/editor/#42bceae61ffdc64afa88e11d4c331bba)  
 [Try it on Codepen!](http://codepen.io/anon/pen/RaNwNd?editors=0010)
 
 Rules Specified for Validation
--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
+// CodeMirror HTMLHint Integration
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})
+
+(function(CodeMirror) {
+  "use strict";
+
+  CodeMirror.registerHelper("lint", "html", function(text) {
+    var found = [], message;
+    if (!window.HTMLHint) return found;
+    var messages = HTMLHint.verify(text, ruleSets);
+    for ( var i = 0; i < messages.length; i++) {
+      message = messages[i];
+      var startLine = message.line -1, endLine = message.line -1, startCol = message.col -1, endCol = message.col;
+      found.push({
+        from: CodeMirror.Pos(startLine, startCol),
+        to: CodeMirror.Pos(endLine, endCol),
+        message: message.message,
+        severity : message.type
+      });
+    }
+    return found;
+  }); 
+});
+
+// ruleSets for HTMLLint
 var ruleSets = {
   "tagname-lowercase": true,
   "attr-lowercase": true,
@@ -9,6 +41,8 @@ var ruleSets = {
   "src-not-empty": true,
   "attr-no-duplication": true
 };
+
+var delay;
 
 // Initialize CodeMirror editor
 var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
@@ -23,3 +57,17 @@ var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   lint: true,
   gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"]
 });
+
+// Live preview
+editor.on('change', function() {
+  clearTimeout(delay);
+  delay = setTimeout(updatePreview, 300);
+});
+function updatePreview() {
+  var previewFrame = document.getElementById('preview');
+  var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+  preview.open();
+  preview.write(editor.getValue());
+  preview.close();
+}
+setTimeout(updatePreview, 300);
